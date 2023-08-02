3cf1fc3a70fb9aec83a90ba92164aff7e5c5e83b
philrussel21 <phil.antiporda21@gmail.com>
2023-08-03 00:21:17 +1000

    chore: remove initial assets. Update tsconfig file. Update tailwind config with container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,13 +8,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				lg: '2.5rem',
+			},
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+			},
+		},
+    extend: {},
   },
   plugins: [],
 }
